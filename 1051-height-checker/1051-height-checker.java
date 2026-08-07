54a70b03b385ee0015bc773c87841e40301238d7
N Jeffwin Daniel <jeffwindaniel18@gmail.com>
2026-08-08 01:20:59 +0530

    Time: 7 ms (39.8%), Space: 44 MB (14.84%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -5,8 +5,6 @@ int[] h2 = Arrays.stream(heights).sorted().toArray();
         int n=heights.length;
         for(int i=0;i<=n-1;i++)
         {
-            //for(int j=i;j<n;j++)
-            //{}
                 if(heights[i]!=h2[i])
                 {
                     c++;
