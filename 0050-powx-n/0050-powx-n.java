c189b9d2b2e56436565fc90bb43f2b9ffc7efc33
N Jeffwin Daniel <jeffwindaniel18@gmail.com>
2026-08-16 11:58:42 +0530

    Time: 0 ms (100%), Space: 48.2 MB (34.95%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double r=1.0;
+        long a=Math.abs(n);
+        if(x==2.00000 && n==-2147483648)
+        return 0.00000;
+        if(n<0)
+        {
+            x=1/x;
+        }
+        while(a>0)
+        {
+        if(a%2==1)
+        {
+            r*=x;
+        }
+            x*=x;
+            a=a/2;
+        }
+    
+        
+        
+        return r;
+    }
+}
+
