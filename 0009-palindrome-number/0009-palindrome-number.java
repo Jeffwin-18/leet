d4da2ad94effe91a1f1abe25205e8f8e3b017e66
N Jeffwin Daniel <jeffwindaniel18@gmail.com>
2026-08-17 22:59:38 +0530

    Time: 5 ms (81.46%), Space: 46 MB (33.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       int r=0;
+       int x1=x;
+       while(x>0)
+       {
+        int d=x%10;
+        r=(r*10)+d;
+        x=x/10;
+       } 
+       if(r==x1)
+       {
+        return true;
+       }
+       else
+       return false;
+    }
+}
