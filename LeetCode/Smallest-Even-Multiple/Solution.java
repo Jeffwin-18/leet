bbc624447ae3522c04157dab5e0511774f964db1
N Jeffwin Daniel <jeffwindaniel18@gmail.com>
2026-08-19 23:16:34 +0530

    Add solution for Smallest Even Multiple

---
+++ LeetCode/Smallest-Even-Multiple/Solution.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public int smallestEvenMultiple(int n) {
+3        if(n%2==0)
+4        {
+5            return n;
+6        }
+7        else
+8        {
+9            return n*2;
+10        }
+11    }
+12}
