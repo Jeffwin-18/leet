4f0177596e68d18cad9e7fa59c28ff7c2c8c4994
N Jeffwin Daniel <jeffwindaniel18@gmail.com>
2026-09-01 22:57:57 +0530

    Update 0242-valid-anagram.java

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        
         char[] charArrayS = s.toCharArray();
         char[] charArrayT = t.toCharArray();
         Arrays.sort(charArrayS);
         Arrays.sort(charArrayT);
+
         return Arrays.equals(charArrayS, charArrayT);
     }
 }
