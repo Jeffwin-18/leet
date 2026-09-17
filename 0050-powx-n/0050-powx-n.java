cd13abd9fa9913a6a7483da3333826973865fe70
N Jeffwin Daniel <jeffwindaniel18@gmail.com>
2026-09-17 12:31:24 +0530

    Time: 0 ms (100%), Space: 47.4 MB (89.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,10 +1,10 @@
 class Solution {
-    public double myPow(double x, int n) {
+    public double myPow(double x, long n) {
         double r=1.0;
         long a=n;
         //if(x==2.00000 && n==-2147483648)
         //return 0.00000;
-        if(n<0)
+        /*if(n<0)
         {
             x=1/x;
             a=-a;
@@ -18,10 +18,27 @@ class Solution {
             x*=x;
             a=a/2;
         }
-    
-        
-        
         return r;
+        above accepted*/
+        if(n==0)
+        {
+            return 1;
+        }
+        if (n<0)
+        {
+            n=-n;
+            x=1/x;
+        }
+        if(n%2==0)
+        {
+            return myPow(x*x,n/2);
+        }
+        else
+        {
+            return x*myPow(x*x,n/2);
+        }
+        
+
     }
 }
 
