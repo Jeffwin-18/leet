6e71405e5561202fa06c81bb95d3e1a76ce87801
N Jeffwin Daniel <jeffwindaniel18@gmail.com>
2026-09-25 14:25:28 +0530

    Add solution for Best Time to Buy and Sell Stock II

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock-II/Solution.java
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock-II/Solution.java
@@ -1,13 +1,13 @@
-1class Solution {
-2    public int maxProfit(int[] prices) {
-3        int max=0;
-4        for(int i=1;i<prices.length;i++)
-5        {
-6            if(prices[i]>prices[i-1])
-7            {
-8                max+=prices[i]-prices[i-1];
-9            }
-10        }
-11        return max;
-12    }
-13}
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                max+=prices[i]-prices[i-1];
+            }
+        }
+        return max;
+    }
+}
